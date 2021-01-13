52ac8aa5573b9cb17798712dc9eee78deed5ea8e
shubhrastogi <54894915+shubhrastogi@users.noreply.github.com>
2021-01-13 22:34:19 +0530

    Add files via upload

---
+++ Sudoku Solver(Project-2)/sudoku.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#define N 9
+using namespace std;
+int grid[N][N] = {
+    {5, 3, 0, 0, 7, 0, 0, 0, 0},
+    {6, 0, 0, 1, 9, 5, 0, 0, 0},
+    {0, 9, 8, 0, 0, 0, 0, 6, 0},
+    {8, 0, 0, 0, 6, 0, 0, 0, 3},
+    {4, 0, 0, 8, 0, 3, 0, 0, 1},
+    {7, 0, 0, 0, 2, 0, 0, 0, 6},
+    {0, 6, 0, 0, 0, 0, 2, 8, 0},
+    {0, 0, 0, 4, 1, 9, 0, 0, 5},
+    {0, 0, 0, 0, 8, 0, 0, 7, 9}
+};
+bool isPresentInCol(int col, int num){ //check number is present in column or not
+    for (int row = 0; row < N; row++)
+        if(grid[row][col] == num)
+            return true;
+    return false;
+}
+bool isPresentInRow(int row, int num){ //check number is present in row or not
+    for(int col = 0; col < N; col++)
+        if(grid[row][col] == num)
+            return true;
+    return false;
+}
+bool isPresentInBox(int boxStartRow, int boxStartCol, int num){
+//check number is present in 3x3 matrix or not
+    for(int row = 0; row < 3; row++)
+        for(int col = 0; col < 3; col++)
+            if(grid[row+boxStartRow][col+boxStartCol] == num)
+                return true;
+    return false;
+}
+void sudokuGrid(){ //print sudoku grid after solve
+    for (int row = 0; row < N; row++){
+        if(row == 0){
+            cout << ' ';
+            for(int i = 0; i<N; i++)
+                cout << "---";
+            cout << endl;
+        }
+        for (int col = 0; col < N; col++){
+            if(col == 0 || col == 3 || col == 6)
+                cout << " | ";
+            cout << grid[row][col] <<" ";
+            if(col == 8)
+                cout << " | ";
+        }
+        if(row == 2 || row == 5 || row == 8){
+            cout << endl << ' ';
+            for(int i = 0; i<N; i++)
+                cout << "---";
+        }
+        cout << endl;
+    }
+}
+bool findEmptyPlace(int &row, int &col){ //get empty location and update row and column
+    for (row = 0; row < N; row++)
+        for (col = 0; col < N; col++)
+            if (grid[row][col] == 0) //marked with 0 is empty
+                return true;
+    return false;
+}
+bool isValidPlace(int row, int col, int num){
+    //when item not found in col, row and current 3x3 box
+    return !isPresentInRow(row, num) && !isPresentInCol(col, num) && !isPresentInBox(row - row%3, col - col%3, num);
+}
+bool solveSudoku(){
+    int row, col;
+    if (!findEmptyPlace(row, col))
+        return true; //when all places are filled
+    for (int num = 1; num <= 9; num++){ //valid numbers are 1 - 9
+        if(isValidPlace(row, col, num)){ //check validation, if yes, put the number in the grid
+            grid[row][col] = num;
+            if (solveSudoku()) //recursively go for other rooms in the grid
+                return true;
+            grid[row][col] = 0; //turn to unassigned space when conditions are not satisfied
+        }
+    }
+    return false;
+}
+int main(){
+    if(solveSudoku() == true)
+        sudokuGrid();
+    else
+        cout << "No solution exists";
+    return 0;
+}
